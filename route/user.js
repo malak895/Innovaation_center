686b5e649058317c9bfd5b09b11d12aea01322be
Hammami Malek <86968591+malak895@users.noreply.github.com>
2023-07-12 20:27:23 +0100

    Delete route directory

--- route/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userController = require('../src/user/userController');
-
-const router = express.Router();
-
-router.route('/users/create').post(userController.createUserControllerFn);
-
-module.exports = router;
